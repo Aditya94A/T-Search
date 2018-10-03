4ee6a777f4d3bf986ead47c59502434e4de148d7
Aditya <aditya@finzoom.in>
2018-10-03 21:59:25 +0530

    isShutDown

--- core/src/main/java/com/adityaanand/t_search/SearchEngine.java
+++ core/src/main/java/com/adityaanand/t_search/SearchEngine.java
@@ -67,4 +67,7 @@ public class SearchEngine {
         requestExecutor.shutdown();
         requestExecutor.shutdownNow();
     }
+    public boolean isShutdown() {
+        return requestExecutor.isShutdown();
+    }
 }
